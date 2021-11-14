6e7ccf05e90adab4ecfc07751a076ee6e11a561f
akshitaanand <akshitaanand@users.noreply.github.com>
2021-11-13 18:46:15 -0600

    home and header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/login"
+import logo from "./utd.jpeg"
+import Dashboard from './components/dashboard';
+
 
 
 function App() {
   return (
-    <Router>
     
-      
-      <Route path = "/" exact component = {Login} />
+    <Router>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" style = {{margin: 30}} href="#">Hello Friend!</a>
+  <img src={logo} width="300" height="100" alt="UTD" style = {{marginRight: 60, marginLeft: 30}}/>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dashboard</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Find a New Friend</a>
+      </li>
+    </ul>
+ 
+  </div>
+</nav>
   
+      <Route path = "/" exact component = {Login} />
+      <Route path = "/dashboard" exact component = {Dashboard} />
     </Router>
 
   );
--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,32 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props){
+        super(props);
+
+        this.onChangeUser = this.onChangeUser.bind(this);
+        this.onChangePass = this.onChangePass.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: "",
+            password: "",
+        }
+    }
+
+    onChangeUser(e){
+        this.setState({username: e.target.value});
+    }
+    onChangePass(e){
+        this.setState({password: e.target.value});
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        if(this.state.username === "aa407" && this.state.password === "password"){
+            this.props.history.push('/dashboard');
+        }
+    }
     render() {
         return (
             <div className = "form-box">
@@ -9,22 +35,22 @@ export default class Login extends Component {
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="text" className="form-control" placeholder="Enter email" value = {this.state.username} onChange = {this.onChangeUser}/>
                 </div>
 
-                <div className="form-group">
+                <div className="form-group" style = {{marginTop: 20}}>
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" value = {this.state.password} onChange = {this.onChangePass}/>
                 </div>
 
                 <div className="form-group">
                     <div className="custom-control custom-checkbox">
                         <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        <label  style = {{marginTop: 20, marginBottom: 20}} className="custom-control-label" htmlFor="customCheck1">Remember me</label>
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                <button type="submit" className="btn btn-primary btn-block" onClick = {this.onSubmit}>Submit</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
